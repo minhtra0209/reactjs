469dabfe322ed4481589422bd1f0f0f15710ebd5
minhtra0209 <tradominh@gmail.com>
2022-08-02 14:19:34 +0700

    RJS101x_01-A_VN_Assignment 1 tradmFX16441

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import { STAFFS } from "./list/staffs";
 class App extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffs: STAFFS,
+    };
+  }
   render() {
     return (
       <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand>Ứng dụng quản lý nhân sự</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
     );
   }
