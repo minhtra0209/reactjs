aeb86d47ae4156997e23b430f8ee88241a8e8766
minhtra0209 <tradominh@gmail.com>
2022-08-27 17:13:52 +0700

    lab03.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Navbar, navbarBrand } from "reactstrap";
+import Menu from "./component/menucomponent";
 class App extends Component {
-
   render() {
     return (
       <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
+        <Navbar dark color="primary">
+          <div className="container">
+            <navbarBrand href="/">Ristorante Con Fusion</navbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
       </div>
     );
   }
